aca23c486fc4de18f581234764196c7f356cf7fd
Atharva Deokar <atharvadeok19@gmail.com>
2025-04-12 23:03:19 +0530

    Completed Day 4: Coded Pricing and Blog pages, tested manually due to server issue

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const navLinks = document.querySelector('.nav-links');
 menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+document.querySelectorAll('.plan-btn, .post-btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+    alert('Clicked!');
+  });
+});
